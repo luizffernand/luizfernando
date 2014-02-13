87e4e950452cccf092a12a0aef500c7e9f001d93
luizfernando <luizffernand@gmail.com>
2014-02-13 18:08:44 -0300

    Tratamento de Erro

--- SEF/SEF/src/sef/module8/activity/SQLExceptionActivity.java
+++ SEF/SEF/src/sef/module8/activity/SQLExceptionActivity.java
@@ -18,12 +18,19 @@ public class SQLExceptionActivity {
 			//1 - put it in a try block and handle ClassNotFoundException
 			
 			
+			try{
 			Class.forName("com.mysql.jdbc.Driver");
+			}catch (ClassNotFoundException e){
+				System.out.println("nao foi possivel encontrar a classe : com.mysql.jdbc.Driver");
+			}
+			try{
 			Connection cn = DriverManager.getConnection(url, user, pass);
 			System.out.println("Connection successfully established! \n");
 			
 			cn.close();
-			
+			}catch(SQLException e){
+				System.out.println("nao foi possivel estabelecer conexao com o banco");
+			}
 			//2 - You also need to catch SQLException for it to compile
 			
 	
